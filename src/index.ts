ed2c31ff397294e6d51e3b861be18982bbf3d347
Andrew <andrew.r.pagan@gmail.com>
2023-06-24 08:24:59 -0700

    ESLint and prettier updates

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,64 @@
-import "dotenv/config";
-import express from "express";
-import axios from "axios";
+import axios from 'axios';
+import 'dotenv/config';
+import express from 'express';
 
-import { addLocation } from "./mongodb";
-import { SLOTS_URL } from "./constants";
-import { getCollection } from "./database";
+import { SLOTS_URL } from './constants';
+import { addLocation, getLocationCollection } from './mongodb';
+
+import type { Collection, Document } from 'mongodb';
 
 const app: express.Express = express();
+const locationCollection: Collection<Document> | undefined =
+  getLocationCollection();
 
-getCollection().then((collection) => {
-  app.get("/location/:locationId", async (req, res) => {
-    const { locationId } = req.params ?? {};
+app.get('/location/:locationId', async (req, res) => {
+  const { locationId } = req.params ?? {};
 
-    if (!locationId) {
-      res.status(500);
-      res.send("You fucked up");
-    }
+  if (!locationId) {
+    res.status(500);
+    res.send('You fucked up');
+  }
 
-    const { data }: axios.AxiosResponse = await axios.get(SLOTS_URL, {
-      params: {
-        orderBy: "soonest",
-        limit: String(1),
-        locationId,
-        minimum: String(1),
-      },
-    });
-
-    const appointment = data[0];
-    const isAvailable =
-      appointment &&
-      appointment?.locationId &&
-      appointment?.startTimestamp &&
-      appointment?.active;
-
-    const result = isAvailable
-      ? {
-          locationId,
-          time: data[0].startTimestamp,
-        }
-      : {};
-
-    addLocation({
-      collection,
+  const { data }: axios.AxiosResponse = await axios.get(SLOTS_URL, {
+    params: {
+      orderBy: 'soonest',
+      limit: String(1),
       locationId,
-      availableTime: appointment?.startTimestamp
-        ? new Date(appointment.startTimestamp).toISOString()
-        : null,
-      isAppointmentAvailable: !!isAvailable,
-    });
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, PUT");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.send(result);
+      minimum: String(1),
+    },
   });
 
-  app.listen(process.env.PORT ?? 3000);
+  const appointment = data[0];
+  const isAvailable =
+    appointment?.locationId &&
+    appointment?.startTimestamp &&
+    appointment?.active;
+  const result = isAvailable
+    ? {
+        locationId,
+        time: data[0].startTimestamp,
+      }
+    : {};
+  const location = {
+    locationId,
+    availableTime: appointment?.startTimestamp
+      ? new Date(appointment.startTimestamp).toISOString()
+      : null,
+    isAppointmentAvailable: !!isAvailable,
+  };
+
+  addLocation({
+    collection: locationCollection,
+    location,
+  });
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.send(result);
 });
+
+app.listen(process.env.PORT ?? 3000);
--- src/mongodb.ts
+++ src/mongodb.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
+import type { Collection, Document } from "mongodb";
+
 const { DB_URI, DB_NAME, COLLECTION_NAME } = process.env ?? {};
 
 const client = new MongoClient(String(DB_URI), {
@@ -11,26 +13,39 @@ const client = new MongoClient(String(DB_URI), {
   },
 });
 
+const getLocationCollection = (): Collection<Document> | undefined => {
+  try {
+    const database = client.db(String(DB_NAME));
+    const collection = database.collection(String(COLLECTION_NAME));
+    console.log("Connected to database and collection");
+    return collection;
+  } catch (err) {
+    console.log("Error when connecting to collection");
+    client.close();
+  }
+};
+
 type LocationSchema = {
-  collection: any;
   locationId: string;
   availableTime: string | null;
   isAppointmentAvailable: boolean;
 };
-export async function addLocation({
-  collection,
-  locationId,
-  availableTime,
-  isAppointmentAvailable,
-}: LocationSchema) {
+type Params = {
+  collection: Collection<Document> | undefined;
+  location: LocationSchema;
+};
+const addLocation = async ({ collection, location }: Params) => {
+  if (!collection) {
+    return;
+  }
   try {
     await collection.insertOne({
-      locationId,
-      availableTime,
-      isAppointmentAvailable,
+      ...location,
       timestamp: new Date().toISOString(),
     });
   } catch (err) {
-    console.log(`Error occured for location ${locationId}: ${err}`);
+    console.log(`Error occured for location ${location.locationId}: ${err}`);
   }
-}
+};
+
+export { addLocation, getLocationCollection };
